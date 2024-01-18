8435f8c396dca6b6fca7148dddefa676cd198622
oumatoulacen <oumatoulacen@gmail.com>
2024-01-18 02:59:02 +0100

    creates a table user/  Best band ever!

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -2,4 +2,4 @@
 -- Context: Calculate/compute something is always power intensive… better to distribute the load!
 SELECT origin, SUM(fans) AS nb_fans
 FROM metal_bands
-GROUP BY origin;
+GROUP BY origin ORDER BY NOT NULL;
