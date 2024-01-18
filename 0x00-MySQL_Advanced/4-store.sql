b6b1dae50edb0ee4e84b557d759ada4205b698af
oumatoulacen <oumatoulacen@gmail.com>
2024-01-18 04:01:50 +0100

    creates a table user/  Best band ever!

---
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -0,0 +1,14 @@
+-- SQL script that creates a trigger that decreases the quantity of an item after adding a new order.
+-- Quantity in the table items can be negative.
+-- Context: Updating multiple tables for one action from your application can generate
+-- issue: network disconnection, crash, etc… to keep your data in a good shape, let MySQL do it for you!
+DELIMITER //
+CREATE TRIGGER trigger_on_add AFTER INSERT ON TABLE orders
+FOR EACH ROW
+BEGIN
+    DECLARE item_quantity INT;
+    -- Get the current quantity of the item
+    SELECT quantity INTO item_quantity FROM items WHERE name = NEW.item_name;
+    UPDATE items SET quantity = item_quantity - NEW.number WHERE name = NEW.item_name;
+END//
+DELIMITER ;
