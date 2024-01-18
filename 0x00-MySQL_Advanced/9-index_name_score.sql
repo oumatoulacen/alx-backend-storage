b4c9cfa843c4f60330c2293b2ab01164426f4bed
oumatoulacen <oumatoulacen@gmail.com>
2024-01-18 20:52:22 +0100

    Optimize simple search and score

--- 0x00-MySQL_Advanced/9-index_name_score.sql
+++ 0x00-MySQL_Advanced/9-index_name_score.sql
@@ -2,4 +2,4 @@
 -- Requirements:
     -- Import this table dump: names.sql.zip
     -- Only the first letter of name AND score must be indexed
-CREATE INDEX idx_name_first ON names(name(1), score)
+CREATE INDEX idx_name_first ON names(name(1), score);
