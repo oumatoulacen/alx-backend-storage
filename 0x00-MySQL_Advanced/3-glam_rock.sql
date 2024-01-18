5c85bf710cfc9d051aadc049b386c0c9185a4350
oumatoulacen <oumatoulacen@gmail.com>
2024-01-18 03:22:48 +0100

    creates a table user/  Best band ever!

---
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,5 @@
+-- SQL script that lists all bands with Glam rock as their main style, ranked by their longevity
+-- Context: Calculate/compute something is always power intensive… better to distribute the load!
+SELECT band_name, (IFNULL(formed, 2022) - IFNULL(split, 2022)) as lifespan
+FROM metal_bands where style = "Glam rock" and formed < 2022
+ORDER BY lifespan DESC;
