d12772cda8c42015038ddbd8fc457daee7cc6a3c
oumatoulacen <oumatoulacen@gmail.com>
2024-01-18 04:08:41 +0100

    creates a table user/  Best band ever!

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -3,12 +3,13 @@
 -- Context: Updating multiple tables for one action from your application can generate
 -- issue: network disconnection, crash, etc… to keep your data in a good shape, let MySQL do it for you!
 DELIMITER //
-CREATE TRIGGER trigger_on_add AFTER INSERT ON TABLE orders
+CREATE TRIGGER trigger_on_add AFTER INSERT ON orders
 FOR EACH ROW
 BEGIN
     DECLARE item_quantity INT;
     -- Get the current quantity of the item
     SELECT quantity INTO item_quantity FROM items WHERE name = NEW.item_name;
-    UPDATE items SET quantity = item_quantity - NEW.number WHERE name = NEW.item_name;
-END//
+    UPDATE items SET quantity = quantity - NEW.number WHERE name = NEW.item_name;
+END;
+//
 DELIMITER ;
