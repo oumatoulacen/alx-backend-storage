0d07ef0e4a899ce95a1556954f0929810ac4b61a
oumatoulacen <oumatoulacen@gmail.com>
2024-01-24 00:09:57 +0100

    stats about Nginx logs stored in MongoDB

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,1 +1,24 @@
 #!/usr/bin/python3
+'''stats about Nginx logs stored in MongoDB'''
+
+
+from pymongo import MongoClient
+
+
+def nginx_stats():
+    '''provides some stats about Nginx logs stored in MongoDB'''
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    db = client.logs
+    collection = db.nginx
+    print(collection.count_documents({}), "logs")
+    print("Methods:")
+    print('\tmethod GET: ', collection.count_documents({'method': 'GET'}))
+    print('\tmethod POST: ', collection.count_documents({'method': 'POST'}))
+    print('\tmethod Delete: ', collection.count_documents({'method': 'Delete'}))
+    print('\tmethod PATCH: ', collection.count_documents({'method': 'PATCH'}))
+    print('\tmethod PUT: ', collection.count_documents({'method': 'PUT'}))
+    print(collection.count_documents({'method': 'GET', 'path': '/status'}), ' status check')
+
+if __name__ == "__main__":
+    '''run as main'''
+    nginx_stats()
