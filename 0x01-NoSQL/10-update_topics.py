0884707f1268f91576dfc99f0ecd893d61cdb187
oumatoulacen <oumatoulacen@gmail.com>
2024-01-23 23:06:10 +0100

    Change school topics

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -1,1 +1,10 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+'''change all topics of a school document based on the name'''
+
+
+from pymongo import MongoClient
+
+
+def update_topics(mongo_collection, name, topics):
+    '''change all topics of a school document based on the name'''
+    mongo_collection.update_many({"name": name}, {"$set": {"topics": topics}})
