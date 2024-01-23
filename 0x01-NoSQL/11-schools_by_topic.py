a957d204d8467a74c18c9c1782036b98239c9a4f
oumatoulacen <oumatoulacen@gmail.com>
2024-01-23 23:11:57 +0100

    schools based on topic

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -1,1 +1,10 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+'''the list of school having a specific topic'''
+
+
+from pymongo import MongoClient
+
+
+def schools_by_topic(mongo_collection, topic):
+    '''returns the list of school having a specific topic'''
+    return mongo_collection.find({'topics': topic})
