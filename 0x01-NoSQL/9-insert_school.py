823bd2373bd9c9127315e3c6445de0f41dd2bf34
oumatoulacen <oumatoulacen@gmail.com>
2024-01-23 22:58:06 +0100

    inserts a new document in a collection

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -1,1 +1,11 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+'''function that inserts a new document in a collection based on kwargs'''
+
+
+from pymongo import MongoClient
+
+
+def insert_school(mongo_collection,**kwargs):
+    '''function that inserts a new document in a collection based on kwargs'''
+    res = mongo_collection.insert_one({**kwargs})
+    return res.inserted_id
