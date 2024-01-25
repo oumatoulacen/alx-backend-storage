db11abad2d314f28d55457523524d30086def592
oumatoulacen <oumatoulacen@gmail.com>
2024-01-25 02:55:05 +0100

    imply get method!

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -20,18 +20,22 @@ class Cache:
         self._redis.set(random_key, data)
         return random_key
 
-    def get(self, key: str, fn: Callable) -> Union[str, bytes, int, float]:
-        '''Get data from Redis'''
+    def get(
+            self,
+            key: str,
+            fn: Callable = None,
+            ) -> Union[str, bytes, int, float]:
+        '''Retrieves a value from a Redis data storage.
+        '''
         data = self._redis.get(key)
-        if fn is not None:
-            return fn(data)
-        else:
-            return data
+        return fn(data) if fn is not None else data
 
     def get_str(self, key: str) -> str:
-        '''Retrieve string data from Redis'''
-        return self.get(key, lambda d: d.decode("utf-8"))
+        '''Retrieves a string value from a Redis data storage.
+        '''
+        return self.get(key, lambda x: x.decode('utf-8'))
 
     def get_int(self, key: str) -> int:
-        '''Retrieve integer data from Redis '''
-        return self.get(key, lambda d: int(d))
+        '''Retrieves an integer value from a Redis data storage.
+        '''
+        return self.get(key, lambda x: int(x))
