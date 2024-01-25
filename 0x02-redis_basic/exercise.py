0af7d5e9a1ac1e8f67076a3d14decb89962d099e
oumatoulacen <oumatoulacen@gmail.com>
2024-01-25 03:36:52 +0100

    Incrementing values

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,11 +1,19 @@
 #!/usr/bin/env python3
 '''Writing strings to Redis'''
 
-
+from functools import wraps
 from typing import Callable, Union
 import redis
 import uuid
 
+def count_calls(method: Callable) -> Callable:
+    '''Count calls decorator'''
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        '''Wrapper'''
+        self._redis.incr(method.__qualname__)
+        return method(self, *args, **kwargs)
+    return wrapper
 
 class Cache:
     '''Cache class'''
@@ -14,6 +22,7 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         '''Store data in Redis'''
         random_key = str(uuid.uuid4())
